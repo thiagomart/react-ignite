1ca26e952360628299941a509f971ab1c2c97731
thiagomart <thiagomsl@gmail.com>
2021-08-04 21:29:34 -0300

    chore(project): add typescript in project

--- src/components/RepositoryList/RepositoryList.tsx
+++ src/components/RepositoryList/RepositoryList.tsx
@@ -1,19 +1,19 @@
 import { RepositoryItem } from "../RepositoryItem/RepositoryItem"
 import '../../styles/repositories.scss'
 import { useEffect, useState } from "react"
+import { Repository } from "../../interface/state/repository"
 
 //https://api.github.com/users/thiagomart/repos
 
 export function RepositoryList() {
     
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
     useEffect(() => {
-        fetch('https://swapi.dev/api/planets/')
+        fetch('https://api.github.com/users/thiagomart/repos')
         .then(response => response.json())
-        .then(data => setRepositories(data.results))
+        .then(data => setRepositories(data))
 
     }, [])
-console.log(repositories)
     return (
         <section className="repository-list"> 
             <h1> Lista de Repositórios</h1>
